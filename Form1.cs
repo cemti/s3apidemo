1f29784982ead42240f9fc41034e8bbd2a001f09
cemti <cemirtan.cristian@outlook.com>
2025-03-04 12:25:39 +0200

    Added placeholder methods for operations.

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,18 @@ namespace s3apidemo
             InitializeComponent();
         }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            InitializeClient();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            FinalizeClient();
+        }
+
         private void SelectFileButton_Click(object sender, EventArgs e)
         {
             using OpenFileDialog dialog = new();
@@ -22,16 +34,19 @@ namespace s3apidemo
         private void UploadButton_Click(object sender, EventArgs e)
         {
             Debug.WriteLine($"Upload file `{fileTextBox.Text}` to bucket `{targetBucketComboBox.Text}` as object `{objectNameTextBox.Text}`.");
+            AddObject(objectNameTextBox.Text, targetBucketComboBox.Text);
         }
 
         private void CreateBucketButton_Click(object sender, EventArgs e)
         {
             Debug.WriteLine($"Create bucket `{bucketNameTextBox.Text}`.");
+            AddBucket(bucketNameTextBox.Text);
         }
 
         private void DeleteBucketButton_Click(object sender, EventArgs e)
         {
             Debug.WriteLine($"Delete bucket `{bucketToDeleteComboBox.Text}`.");
+            DeleteBucket(bucketToDeleteComboBox.Text);
         }
 
         private void SourceBucketComboBox_SelectedIndexChanged(object sender, EventArgs e)
@@ -57,22 +72,25 @@ namespace s3apidemo
             if (objectListBox.SelectedIndex != -1)
             {
                 Debug.WriteLine("Delete the selected object.");
+                DeleteObject(objectNameTextBox.Text, targetBucketComboBox.Text);
             }
         }
 
         private void RenameButton_Click(object sender, EventArgs e)
         {
-            if (objectListBox.SelectedIndex != -1)
+            if (objectListBox.SelectedValue is { } selectedValue)
             {
                 Debug.WriteLine($"Rename the selected object to `{newObjectNameTextBox.Text}`.");
+                RenameObject((string)selectedValue!, newObjectNameTextBox.Text, sourceBucketComboBox.Text);
             }
         }
 
         private void MoveObjectButton_Click(object sender, EventArgs e)
         {
-            if (objectListBox.SelectedIndex != -1)
-            {
+            if (objectListBox.SelectedValue is { } selectedValue)
+            {                
                 Debug.WriteLine($"Move the selected object from `{sourceBucketComboBox.Text}` to `{moveToBucketComboBox.Text}`.");
+                MoveObject((string)selectedValue!, sourceBucketComboBox.Text, moveToBucketComboBox.Text);
             }
         }
     }
---
+++ FormOperations.cs
@@ -0,0 +1,103 @@
+﻿using System.ComponentModel;
+
+namespace s3apidemo
+{
+    partial class Form1
+    {
+        private readonly BindingList<KeyValuePair<string, BindingList<string>>> _buckets =
+        [
+            new("sample1", ["s1-object1"]),
+            new("sample2", ["s2-object1"])
+        ];
+
+        private void InitializeClient()
+        {
+            void PrepareComboBox(ComboBox comboBox)
+            {
+                comboBox.DataSource = new BindingSource(_buckets, null!);
+                comboBox.DisplayMember = "Key";
+            }
+
+            PrepareComboBox(sourceBucketComboBox);
+            PrepareComboBox(targetBucketComboBox);
+            PrepareComboBox(bucketToDeleteComboBox);
+            PrepareComboBox(moveToBucketComboBox);
+
+            objectListBox.DataSource = new BindingSource(sourceBucketComboBox.DataSource, "Value");
+        }
+
+        private void FinalizeClient()
+        { 
+        }
+
+        private IList<string> GetBucket(string bucketName)
+        {
+            return _buckets.First(x => x.Key == bucketName).Value;
+        }
+
+        private void AddObject(string objectName, string bucketName)
+        {
+            var bucket = GetBucket(bucketName);
+
+            if (bucket.Contains(bucketName))
+            {
+                MessageBox.Show($"Object '{objectName}' already exists in bucket '{bucketName}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            bucket.Add(objectName);
+        }
+
+        private void RenameObject(string oldObjectName, string newObjectName, string bucketName)
+        {
+            var bucket = GetBucket(bucketName);
+
+            if (bucket.Contains(newObjectName))
+            {
+                MessageBox.Show($"Object '{newObjectName}' already exists in bucket '{bucketName}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (bucket.IndexOf(oldObjectName) is not -1 and var index)
+            {
+                bucket[index] = newObjectName;
+            }
+        }
+        
+        private void DeleteObject(string objectName, string bucketName)
+        {
+            GetBucket(bucketName).Remove(objectName);
+        }
+
+        private void MoveObject(string objectName, string sourceBucketName, string destinationBucketName)
+        {
+            if (GetBucket(destinationBucketName).Contains(objectName))
+            {
+                MessageBox.Show($"Object '{objectName}' already exists in '{destinationBucketName}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            DeleteObject(objectName, sourceBucketName);
+            AddObject(objectName, destinationBucketName);
+        }
+
+        private void AddBucket(string bucketName)
+        {
+            if (_buckets.Any(x => x.Key == bucketName))
+            {
+                MessageBox.Show($"Bucket '{bucketName}' already exists.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _buckets.Add(new(bucketName, []));
+        }
+
+        private void DeleteBucket(string bucketName)
+        {
+            if (_buckets.Index().FirstOrDefault(x => x.Item.Key == bucketName) is { Index: var index, Item: ({ }, { }) })
+            {
+                _buckets.RemoveAt(index);
+            }
+        }
+    }
+}
