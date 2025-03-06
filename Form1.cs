795df822b18b0c2ad8cf5091d62148130a5d9f2a
cemti <cemirtan.cristian@outlook.com>
2025-03-06 21:51:28 +0200

    Simplified the namespace use.

--- Form1.cs
+++ Form1.cs
@@ -1,166 +1,165 @@
 using System.ComponentModel;
 using System.Diagnostics;
 
-namespace S3ApiDemo
+namespace S3ApiDemo;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private readonly BindingList<string> _buckets = [], _currentObjects = [];
+
+    public Form1()
     {
-        private readonly BindingList<string> _buckets = [], _currentObjects = [];
+        InitializeComponent();
 
-        public Form1()
+        void PrepareComboBox(ComboBox comboBox)
         {
-            InitializeComponent();
-
-            void PrepareComboBox(ComboBox comboBox)
-            {
-                comboBox.DataSource = new BindingSource(_buckets, null!);
-            }
-
-            PrepareComboBox(sourceBucketComboBox);
-            PrepareComboBox(targetBucketComboBox);
-            PrepareComboBox(bucketToDeleteComboBox);
-            PrepareComboBox(moveToBucketComboBox);
-
-            objectListBox.DataSource = new BindingSource(_currentObjects, null!);
+            comboBox.DataSource = new BindingSource(_buckets, null!);
         }
 
-        protected override async void OnLoad(EventArgs e)
+        PrepareComboBox(sourceBucketComboBox);
+        PrepareComboBox(targetBucketComboBox);
+        PrepareComboBox(bucketToDeleteComboBox);
+        PrepareComboBox(moveToBucketComboBox);
+
+        objectListBox.DataSource = new BindingSource(_currentObjects, null!);
+    }
+
+    protected override async void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+        InitializeClient();
+
+        var buckets = await QueryBuckets();
+
+        _buckets.Clear();
+
+        foreach (var bucket in buckets)
         {
-            base.OnLoad(e);
-            InitializeClient();
+            _buckets.Add(bucket);
+        }
+    }
 
-            var buckets = await QueryBuckets();
+    protected override void OnClosed(EventArgs e)
+    {
+        base.OnClosed(e);
+        FinalizeClient();
+    }
 
-            _buckets.Clear();
+    private void SelectFileButton_Click(object sender, EventArgs e)
+    {
+        using OpenFileDialog dialog = new();
 
-            foreach (var bucket in buckets)
-            {
-                _buckets.Add(bucket);
-            }
+        if (dialog.ShowDialog() == DialogResult.OK)
+        {
+            fileTextBox.Text = dialog.FileName;
+        }
+    }
+
+    private async void UploadButton_Click(object sender, EventArgs e)
+    {
+        Debug.WriteLine($"Upload file `{fileTextBox.Text}` to bucket `{targetBucketComboBox.Text}` as object `{objectNameTextBox.Text}`.");
+
+        if (await AddObject(objectNameTextBox.Text, targetBucketComboBox.Text, fileTextBox.Text))
+        {
+            _currentObjects.Add(objectNameTextBox.Text);
+            return;
         }
 
-        protected override void OnClosed(EventArgs e)
+        MessageBox.Show($"Error adding '{objectNameTextBox.Text}' to bucket '{targetBucketComboBox.Text}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+
+    private async void CreateBucketButton_Click(object sender, EventArgs e)
+    {
+        Debug.WriteLine($"Create bucket `{bucketNameTextBox.Text}`.");
+        
+        if (await AddBucket(bucketNameTextBox.Text))
         {
-            base.OnClosed(e);
-            FinalizeClient();
+            _buckets.Add(bucketNameTextBox.Text);
+            return;
         }
 
-        private void SelectFileButton_Click(object sender, EventArgs e)
+        MessageBox.Show($"Error adding bucket '{bucketNameTextBox.Text}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+
+    private async void DeleteBucketButton_Click(object sender, EventArgs e)
+    {
+        Debug.WriteLine($"Delete bucket `{bucketToDeleteComboBox.Text}`.");
+        
+        if (await DeleteBucket(bucketToDeleteComboBox.Text))
         {
-            using OpenFileDialog dialog = new();
+            _buckets.RemoveAt(bucketToDeleteComboBox.SelectedIndex);
+            return;
+        }
+
+        MessageBox.Show($"Error deleting bucket '{bucketToDeleteComboBox.Text}'. Make sure it's empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+
+    private async void SourceBucketComboBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        Debug.WriteLine($"Query objects from bucket `{sourceBucketComboBox.Text}`.");
+        var objects = await QueryObjects(sourceBucketComboBox.Text);
+
+        _currentObjects.Clear();
+        
+        foreach (var obj in objects)
+        {
+            _currentObjects.Add(obj);
+        }
+    }
+
+    private async void DownloadButton_Click(object sender, EventArgs e)
+    {
+        if (objectListBox.SelectedValue is string selectedObject)
+        {
+            using SaveFileDialog dialog = new();
 
             if (dialog.ShowDialog() == DialogResult.OK)
             {
-                fileTextBox.Text = dialog.FileName;
+                Debug.WriteLine("Save the selected object content.");
+                await DownloadObject(selectedObject, sourceBucketComboBox.Text, dialog.FileName);
             }
         }
+    }
 
-        private async void UploadButton_Click(object sender, EventArgs e)
+    private async void DeleteButton_Click(object sender, EventArgs e)
+    {
+        if (objectListBox is { SelectedValue: string selectedObject, SelectedIndex: var selectedIndex })
         {
-            Debug.WriteLine($"Upload file `{fileTextBox.Text}` to bucket `{targetBucketComboBox.Text}` as object `{objectNameTextBox.Text}`.");
+            Debug.WriteLine("Delete the selected object.");
 
-            if (await AddObject(objectNameTextBox.Text, targetBucketComboBox.Text, fileTextBox.Text))
+            if (await DeleteObject(selectedObject, sourceBucketComboBox.Text))
             {
-                _currentObjects.Add(objectNameTextBox.Text);
+                _currentObjects.RemoveAt(selectedIndex);
+            }
+        }
+    }
+
+    private async void RenameButton_Click(object sender, EventArgs e)
+    {
+        if (objectListBox is { SelectedValue: string selectedObject, SelectedIndex: var selectedIndex })
+        {
+            Debug.WriteLine($"Rename the selected object to `{newObjectNameTextBox.Text}`.");
+
+            if (await RenameObject(selectedObject, newObjectNameTextBox.Text, sourceBucketComboBox.Text))
+            {
+                _currentObjects[selectedIndex] = newObjectNameTextBox.Text;
                 return;
             }
 
-            MessageBox.Show($"Error adding '{objectNameTextBox.Text}' to bucket '{targetBucketComboBox.Text}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show($"Error adding '{newObjectNameTextBox.Text}' to bucket '{sourceBucketComboBox.Text}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
+    }
 
-        private async void CreateBucketButton_Click(object sender, EventArgs e)
-        {
-            Debug.WriteLine($"Create bucket `{bucketNameTextBox.Text}`.");
-            
-            if (await AddBucket(bucketNameTextBox.Text))
+    private async void MoveObjectButton_Click(object sender, EventArgs e)
+    {
+        if (objectListBox is { SelectedValue: string selectedObject, SelectedIndex: var selectedIndex })
+        {                
+            Debug.WriteLine($"Move the selected object from `{sourceBucketComboBox.Text}` to `{moveToBucketComboBox.Text}`.");
+
+            if (await MoveObject(selectedObject, sourceBucketComboBox.Text, moveToBucketComboBox.Text)
+                && sourceBucketComboBox.Text != moveToBucketComboBox.Text)
             {
-                _buckets.Add(bucketNameTextBox.Text);
-                return;
-            }
-
-            MessageBox.Show($"Error adding bucket '{bucketNameTextBox.Text}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-
-        private async void DeleteBucketButton_Click(object sender, EventArgs e)
-        {
-            Debug.WriteLine($"Delete bucket `{bucketToDeleteComboBox.Text}`.");
-            
-            if (await DeleteBucket(bucketToDeleteComboBox.Text))
-            {
-                _buckets.RemoveAt(bucketToDeleteComboBox.SelectedIndex);
-                return;
-            }
-
-            MessageBox.Show($"Error deleting bucket '{bucketToDeleteComboBox.Text}'. Make sure it's empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-
-        private async void SourceBucketComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            Debug.WriteLine($"Query objects from bucket `{sourceBucketComboBox.Text}`.");
-            var objects = await QueryObjects(sourceBucketComboBox.Text);
-
-            _currentObjects.Clear();
-            
-            foreach (var obj in objects)
-            {
-                _currentObjects.Add(obj);
-            }
-        }
-
-        private async void DownloadButton_Click(object sender, EventArgs e)
-        {
-            if (objectListBox.SelectedValue is string selectedObject)
-            {
-                using SaveFileDialog dialog = new();
-
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    Debug.WriteLine("Save the selected object content.");
-                    await DownloadObject(selectedObject, sourceBucketComboBox.Text, dialog.FileName);
-                }
-            }
-        }
-
-        private async void DeleteButton_Click(object sender, EventArgs e)
-        {
-            if (objectListBox is { SelectedValue: string selectedObject, SelectedIndex: var selectedIndex })
-            {
-                Debug.WriteLine("Delete the selected object.");
-
-                if (await DeleteObject(selectedObject, sourceBucketComboBox.Text))
-                {
-                    _currentObjects.RemoveAt(selectedIndex);
-                }
-            }
-        }
-
-        private async void RenameButton_Click(object sender, EventArgs e)
-        {
-            if (objectListBox is { SelectedValue: string selectedObject, SelectedIndex: var selectedIndex })
-            {
-                Debug.WriteLine($"Rename the selected object to `{newObjectNameTextBox.Text}`.");
-
-                if (await RenameObject(selectedObject, newObjectNameTextBox.Text, sourceBucketComboBox.Text))
-                {
-                    _currentObjects[selectedIndex] = newObjectNameTextBox.Text;
-                    return;
-                }
-
-                MessageBox.Show($"Error adding '{newObjectNameTextBox.Text}' to bucket '{sourceBucketComboBox.Text}'.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private async void MoveObjectButton_Click(object sender, EventArgs e)
-        {
-            if (objectListBox is { SelectedValue: string selectedObject, SelectedIndex: var selectedIndex })
-            {                
-                Debug.WriteLine($"Move the selected object from `{sourceBucketComboBox.Text}` to `{moveToBucketComboBox.Text}`.");
-
-                if (await MoveObject(selectedObject, sourceBucketComboBox.Text, moveToBucketComboBox.Text)
-                    && sourceBucketComboBox.Text != moveToBucketComboBox.Text)
-                {
-                    _currentObjects.RemoveAt(selectedIndex);
-                }
+                _currentObjects.RemoveAt(selectedIndex);
             }
         }
     }
--- FormOperations.cs
+++ FormOperations.cs
@@ -4,101 +4,100 @@ using Amazon.Runtime;
 using Amazon.S3;
 using System.Net;
 
-namespace S3ApiDemo
+namespace S3ApiDemo;
+
+partial class Form1
 {
-    partial class Form1
+    private AmazonS3Client _client = null!;
+
+    private void InitializeClient()
     {
-        private AmazonS3Client _client = null!;
+        var accessKey = Interaction.InputBox("Access key", "AWS IAM");
+        var secretKey = Interaction.InputBox("Secret access key", "AWS IAM");
+        _client = new(new BasicAWSCredentials(accessKey, secretKey), RegionEndpoint.USEast1);
+    }
 
-        private void InitializeClient()
+    private void FinalizeClient()
+    {
+        _client.Dispose();
+    }
+
+    private async Task<IEnumerable<string>> QueryBuckets()
+    {
+        var buckets = await _client.ListBucketsAsync();
+        return buckets.Buckets.Select(x => x.BucketName);
+    }
+
+    private async Task<IEnumerable<string>> QueryObjects(string bucketName)
+    {
+        if (bucketName == "")
         {
-            var accessKey = Interaction.InputBox("Access key", "AWS IAM");
-            var secretKey = Interaction.InputBox("Secret access key", "AWS IAM");
-            _client = new(new BasicAWSCredentials(accessKey, secretKey), RegionEndpoint.USEast1);
+            return [];
         }
 
-        private void FinalizeClient()
+        var objects = await _client.ListObjectsAsync(bucketName);
+        return objects.S3Objects.Select(x => x.Key);
+    }
+
+    private async Task<bool> AddObject(string objectName, string bucketName, string filePath)
+    {
+        var response = await _client.PutObjectAsync(new()
         {
-            _client.Dispose();
+            BucketName = bucketName,
+            Key = objectName,
+            FilePath = filePath
+        });
+
+        return response.HttpStatusCode == HttpStatusCode.OK;
+    }
+
+    private async Task<bool> RenameObject(string oldObjectName, string newObjectName, string bucketName)
+    {
+        var response = await _client.CopyObjectAsync(bucketName, oldObjectName, bucketName, newObjectName);
+
+        if (response.HttpStatusCode != HttpStatusCode.OK)
+        {
+            return false;
         }
 
-        private async Task<IEnumerable<string>> QueryBuckets()
+        _ = await _client.DeleteObjectAsync(bucketName, oldObjectName);
+        return true;
+    }
+
+    private async Task DownloadObject(string objectName, string bucketName, string filePath)
+    {
+        var response = await _client.GetObjectAsync(bucketName, objectName);
+        await response.WriteResponseStreamToFileAsync(filePath, false, CancellationToken.None);
+    }
+
+    private async Task<bool> DeleteObject(string objectName, string bucketName)
+    {
+        _ = await _client.DeleteObjectAsync(bucketName, objectName);
+        return true;
+    }
+
+    private async Task<bool> MoveObject(string objectName, string sourceBucketName, string destinationBucketName)
+    {
+        var response = await _client.CopyObjectAsync(sourceBucketName, objectName, destinationBucketName, objectName);
+
+        if (response.HttpStatusCode != HttpStatusCode.OK)
         {
-            var buckets = await _client.ListBucketsAsync();
-            return buckets.Buckets.Select(x => x.BucketName);
+            return false;
         }
 
-        private async Task<IEnumerable<string>> QueryObjects(string bucketName)
-        {
-            if (bucketName == "")
-            {
-                return [];
-            }
+        _ = await _client.DeleteObjectAsync(sourceBucketName, objectName);
+        return true;
+    }
 
-            var objects = await _client.ListObjectsAsync(bucketName);
-            return objects.S3Objects.Select(x => x.Key);
-        }
+    private async Task<bool> AddBucket(string bucketName)
+    {
+        var response = await _client.PutBucketAsync(bucketName);
+        return response.HttpStatusCode == HttpStatusCode.OK;
+    }
 
-        private async Task<bool> AddObject(string objectName, string bucketName, string filePath)
-        {
-            var response = await _client.PutObjectAsync(new()
-            {
-                BucketName = bucketName,
-                Key = objectName,
-                FilePath = filePath
-            });
-
-            return response.HttpStatusCode == HttpStatusCode.OK;
-        }
-
-        private async Task<bool> RenameObject(string oldObjectName, string newObjectName, string bucketName)
-        {
-            var response = await _client.CopyObjectAsync(bucketName, oldObjectName, bucketName, newObjectName);
-
-            if (response.HttpStatusCode != HttpStatusCode.OK)
-            {
-                return false;
-            }
-
-            _ = await _client.DeleteObjectAsync(bucketName, oldObjectName);
-            return true;
-        }
-
-        private async Task DownloadObject(string objectName, string bucketName, string filePath)
-        {
-            var response = await _client.GetObjectAsync(bucketName, objectName);
-            await response.WriteResponseStreamToFileAsync(filePath, false, CancellationToken.None);
-        }
-
-        private async Task<bool> DeleteObject(string objectName, string bucketName)
-        {
-            _ = await _client.DeleteObjectAsync(bucketName, objectName);
-            return true;
-        }
-
-        private async Task<bool> MoveObject(string objectName, string sourceBucketName, string destinationBucketName)
-        {
-            var response = await _client.CopyObjectAsync(sourceBucketName, objectName, destinationBucketName, objectName);
-
-            if (response.HttpStatusCode != HttpStatusCode.OK)
-            {
-                return false;
-            }
-
-            _ = await _client.DeleteObjectAsync(sourceBucketName, objectName);
-            return true;
-        }
-
-        private async Task<bool> AddBucket(string bucketName)
-        {
-            var response = await _client.PutBucketAsync(bucketName);
-            return response.HttpStatusCode == HttpStatusCode.OK;
-        }
-
-        private async Task<bool> DeleteBucket(string bucketName)
-        {
-            _ = await _client.DeleteBucketAsync(bucketName);
-            return true;
-        }
+    private async Task<bool> DeleteBucket(string bucketName)
+    {
+        _ = await _client.DeleteBucketAsync(bucketName);
+        return true;
     }
 }
