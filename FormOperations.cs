a9ed841c29fd64d19c128def0a644a09e12010cc
cemti <cemirtan.cristian@outlook.com>
2025-03-06 21:47:29 +0200

    The `_client` field ensured to not be null.

--- FormOperations.cs
+++ FormOperations.cs
@@ -8,7 +8,7 @@ namespace S3ApiDemo
 {
     partial class Form1
     {
-        private AmazonS3Client? _client;
+        private AmazonS3Client _client = null!;
 
         private void InitializeClient()
         {
@@ -19,24 +19,18 @@ namespace S3ApiDemo
 
         private void FinalizeClient()
         {
-            _client?.Dispose();
-            _client = null;
+            _client.Dispose();
         }
 
         private async Task<IEnumerable<string>> QueryBuckets()
         {
-            if (_client is null)
-            {
-                return [];
-            }
-
             var buckets = await _client.ListBucketsAsync();
             return buckets.Buckets.Select(x => x.BucketName);
         }
 
         private async Task<IEnumerable<string>> QueryObjects(string bucketName)
         {
-            if (_client is null || bucketName == "")
+            if (bucketName == "")
             {
                 return [];
             }
@@ -47,11 +41,6 @@ namespace S3ApiDemo
 
         private async Task<bool> AddObject(string objectName, string bucketName, string filePath)
         {
-            if (_client is null)
-            {
-                return false;
-            }
-
             var response = await _client.PutObjectAsync(new()
             {
                 BucketName = bucketName,
@@ -64,11 +53,6 @@ namespace S3ApiDemo
 
         private async Task<bool> RenameObject(string oldObjectName, string newObjectName, string bucketName)
         {
-            if (_client is null)
-            {
-                return false;
-            }
-
             var response = await _client.CopyObjectAsync(bucketName, oldObjectName, bucketName, newObjectName);
 
             if (response.HttpStatusCode != HttpStatusCode.OK)
@@ -82,33 +66,18 @@ namespace S3ApiDemo
 
         private async Task DownloadObject(string objectName, string bucketName, string filePath)
         {
-            if (_client is null)
-            {
-                return;
-            }
-
             var response = await _client.GetObjectAsync(bucketName, objectName);
             await response.WriteResponseStreamToFileAsync(filePath, false, CancellationToken.None);
         }
 
         private async Task<bool> DeleteObject(string objectName, string bucketName)
         {
-            if (_client is null)
-            {
-                return false;
-            }
-            
             _ = await _client.DeleteObjectAsync(bucketName, objectName);
             return true;
         }
 
         private async Task<bool> MoveObject(string objectName, string sourceBucketName, string destinationBucketName)
         {
-            if (_client is null)
-            {
-                return false;
-            }
-
             var response = await _client.CopyObjectAsync(sourceBucketName, objectName, destinationBucketName, objectName);
 
             if (response.HttpStatusCode != HttpStatusCode.OK)
@@ -122,22 +91,12 @@ namespace S3ApiDemo
 
         private async Task<bool> AddBucket(string bucketName)
         {
-            if (_client is null)
-            {
-                return false;
-            }
-
             var response = await _client.PutBucketAsync(bucketName);
             return response.HttpStatusCode == HttpStatusCode.OK;
         }
 
         private async Task<bool> DeleteBucket(string bucketName)
         {
-            if (_client is null)
-            {
-                return false;
-            }
-            
             _ = await _client.DeleteBucketAsync(bucketName);
             return true;
         }
